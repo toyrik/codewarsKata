6abf099501032cf330955434d539cf9f2a8e0415
Михаил <toyrik@gmail.com>
2020-05-24 16:23:20 +0300

    Add solution Convert string -to camel-case

---
+++ php/Convert-string-to-camel-case.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Complete the method/function so that it converts dash/underscore delimited  * words into camel casing. The first word within the output should be 
+ * capitalized only if the original word was capitalized (known as Upper Camel * Case, also often referred to as Pascal case).
+ *
+ * Examples
+ * 
+ * toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
+ * 
+ * toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
+ */
+function toCamelCase($str){
+  $str = str_replace('-', '_', $str);
+  $splitString = explode("_", $str);
+  $resString = "";
+  foreach ($splitString as $key => $word ){
+    $slice = $word;
+    if ($key != 0){
+      $slice = ucwords($word, "_");
+    }
+    
+    $resString = $resString . $slice;
+  }
+
+  return $resString;
+}
+
+
+echo "<pre>";
+var_dump($splitString);
+print_r(toCamelCase("the_stealth_warrior"));
+echo "</pre>";
